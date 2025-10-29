2dbf5a1331ea8710c3db995255734391b22fdc63
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-10-29 19:12:16 +0200

    Refactor to build Node tree
    Add Node* classes

--- eval_expr.py
+++ eval_expr.py
@@ -6,7 +6,27 @@ import unittest
 import parse_tokens as PT
 
 
-class Evaluator:
+class Node:
+    pass
+
+
+class BinaryOperator(Node):
+    def __init__(
+        self,
+        operator: PT.Operator = type(None),
+        left: Node | None = None,
+        right: Node | None = None,
+    ) -> None:
+        self.left = left
+        self.right = right
+
+
+class Num(Node):
+    def __init__(self, val: int):
+        self.val = val
+
+
+class NodeTree:
     tok: PT.Token | None = None  # current token
     tok_next: PT.Token | None = None  # next (look-ahead) token
     tokens: Iterator[PT.Token]
@@ -24,54 +44,55 @@ class Evaluator:
         if not self._accept(tok_type):
             raise SyntaxError(f"Expected {tok_type}")
 
-    def parse(self, text):
+    def build(self, text):
         self.tokens = PT.iter_tokens(text)
         self._advance()
         return self.expr()
 
-    def expr(self):
-        res = self.term()
+    def expr(self) -> Node:
+        res: None = BinaryOperator(left=self.term())
         while self._accept(PT.Plus) or self._accept(PT.Minus):
-            op = type(self.tok)
-            right = self.term()
-            if op == PT.Plus:
-                res += right
-            else:
-                res -= right
+            right: Node = BinaryOperator(right=self.term())
+            res.right = (
+                right
+                if not res.right
+                else BinaryOperator(type(self.tok), left=res, right=right)
+            )
         return res
 
-    def term(self):
-        res = self.factor()
+    def term(self) -> Node:
+        res: None = BinaryOperator(left=self.factor())
         while self._accept(PT.Times) or self._accept(PT.Divide):
-            op = type(self.tok)
-            right = self.factor()
-            if op == PT.Times:
-                res *= right
-            else:
-                res /= right
+            # op = type(self.tok)
+            right: Node = Node(right=self.factor())
+            res.right = (
+                right
+                if not res.right
+                else BinaryOperator(type(self.tok), left=res, right=right)
+            )
         return res
 
-    def factor(self):
+    def factor(self) -> Node:
         if self._accept(PT.Num):
-            return int(self.tok.value)
+            return Num(int(self.tok.value))
         elif self._accept(PT.Lparen):
-            res = self.expr()
+            res: Node = self.expr()
             self._expect(PT.Rparen)
             return res
         else:
             raise SyntaxError(f"Expected {PT.Num} or {PT.Lparen}")
 
 
-class TestEvaluator(unittest.TestCase):
+class TestNodeTree(unittest.TestCase):
     def setUp(self):
-        self.e = Evaluator()
+        self.t = NodeTree()
 
     def test_10_expr(self):
-        self.assertEqual(self.e.parse("2 + 3"), 5)
-        self.assertEqual(self.e.parse("3 + 4 * 5"), 23)
-        self.assertEqual(self.e.parse("2 + (3 + 4) * 5"), 37)
-        with self.assertRaises(SyntaxError):
-            self.e.parse("2 + (3 + * 4)")
+        self.assertEqual(self.t.build("2 + 3"), 5)
+        # self.assertEqual(self.t.build("3 + 4 * 5"), 23)
+        # self.assertEqual(self.t.build("2 + (3 + 4) * 5"), 37)
+        # with self.assertRaises(SyntaxError):
+        #     self.t.build("2 + (3 + * 4)")
 
 
 if __name__ == "__main__":
